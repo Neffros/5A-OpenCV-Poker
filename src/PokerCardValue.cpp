8cd1174c327914e9c77145db1400dab75b265b58
Cyphall <cyphallbraus@gmail.com>
2022-02-11 14:22:10 +0100

    removed dense features

--- src/PokerCardValue.cpp
+++ src/PokerCardValue.cpp
@@ -1,20 +1,22 @@
 #include "../include/PokerCardValue.h"
 
 PokerCardValue::PokerCardValue(cv::Mat&& pixelData, cv::FeatureDetector& featureDetector, PokerCard::Value value):
-Image(std::forward<cv::Mat>(pixelData)), _value(value)
+Image(std::forward<cv::Mat>(pixelData), &featureDetector), _value(value)
 {
-	int width = _pixelData.size().width;
-	int height = _pixelData.size().height;
-	
-	for (int y = 0; y < height; y++)
-	{
-		for (int x = 0; x < width; x++)
-		{
-			_keyPoints.emplace_back(static_cast<float>(x), static_cast<float>(y), 5.0f);
-		}
-	}
-	
-	featureDetector.compute(_pixelData, _keyPoints, _descriptors);
+//	int width = _pixelData.size().width;
+//	int height = _pixelData.size().height;
+//
+//	int step = 5;
+//
+//	for (int y = 0; y < height - step; y += step)
+//	{
+//		for (int x = 0; x < width - step; x += step)
+//		{
+//			_keyPoints.emplace_back(static_cast<float>(x), static_cast<float>(y), static_cast<float>(step));
+//		}
+//	}
+//
+//	featureDetector.compute(_pixelData, _keyPoints, _descriptors);
 }
 
 PokerCard::Value PokerCardValue::getValue() const
