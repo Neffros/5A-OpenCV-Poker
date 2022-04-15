b402cb6a2311e4358209efc54145b5726e241257
Quentin <pamartqu@gmail.com>
2022-04-15 18:00:06 +0200

    namespace + .h for main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,58 +1,58 @@
+#include "Main.h"
 #include <iostream>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 
 #include "../include/PokerTable.h"
 #include "../include/PokerAnalyzer.h"
 
 #include <filesystem>
 #include <fmt/format.h>
-#include "SolutionAResult.h"
 
-SolutionAResult compute(const cv::Mat &testImage) {
-    std::filesystem::path resourcePath = "resources";
-    std::filesystem::path pokerTablesFolder = "pokerTables";
-    std::filesystem::path cardsImgName = "cards.jpg";
+namespace solA {
+    SolutionAResult compute(const cv::Mat &testImage) {
+        std::filesystem::path resourcePath = "resources";
+        std::filesystem::path pokerTablesFolder = "pokerTables";
+        std::filesystem::path cardsImgName = "cards.jpg";
 
-    PokerAnalyzer pokerAnalyzer(resourcePath / cardsImgName);
+        PokerAnalyzer pokerAnalyzer(resourcePath / cardsImgName);
 
-    std::vector<PokerTable> pokerTables;
-    for (const auto &entry: std::filesystem::directory_iterator(resourcePath / pokerTablesFolder)) {
-        std::filesystem::path path = entry.path();
+        std::vector<PokerTable> pokerTables;
+        for (const auto &entry: std::filesystem::directory_iterator(resourcePath / pokerTablesFolder)) {
+            std::filesystem::path path = entry.path();
 
-        std::string ext = path.extension().generic_string();
-        if (ext != ".png" && ext != ".jpeg" && ext != ".BMP" && ext != ".TGA" && ext != ".jpg") {
-            std::cout << fmt::format("{} is not a compatible image", path.generic_string().c_str()) << std::endl;
-            continue;
+            std::string ext = path.extension().generic_string();
+            if (ext != ".png" && ext != ".jpeg" && ext != ".BMP" && ext != ".TGA" && ext != ".jpg") {
+                std::cout << fmt::format("{} is not a compatible image", path.generic_string().c_str()) << std::endl;
+                continue;
+            }
+
+            pokerTables.emplace_back(pokerAnalyzer.loadPokerTable(cv::imread(path.generic_string())));
+            //break; //TODO: remove
         }
 
-        pokerTables.emplace_back(pokerAnalyzer.loadPokerTable(cv::imread(path.generic_string())));
-        //break; //TODO: remove
+        /*for (const PokerCard& card : pokerAnalyzer.getCards())
+        {
+            if (card.getType() == PokerCard::Type::Clubs && card.getValue() == PokerCard::Value::Two)
+            {
+                cv::imshow("card image", card.getOriginalPixelData());
+
+                cv::Mat output;
+                cv::drawKeypoints(card.getOriginalPixelData(), card.getKeyPoints(), output);
+                cv::imshow("card keypoints", output);
+            }
+            if(card.getType() == PokerCard::Type::Clubs && card.getValue() == PokerCard::Value::Nine)
+            {
+                cv::imshow("processedCard", card.getPreprocessedPixelData());
+            }
+        }*/
+
+        pokerAnalyzer.analyze(pokerTables[10]);
+        //cv::imwrite("processedTable.jpg", pokerTables[0].getPreprocessedPixelData());
+
+        //cv::waitKey();
+
+        SolutionAResult results(pokerAnalyzer.getCards());
+        return results;
     }
-
-    /*for (const PokerCard& card : pokerAnalyzer.getCards())
-    {
-        if (card.getType() == PokerCard::Type::Clubs && card.getValue() == PokerCard::Value::Two)
-        {
-            cv::imshow("card image", card.getOriginalPixelData());
-
-            cv::Mat output;
-            cv::drawKeypoints(card.getOriginalPixelData(), card.getKeyPoints(), output);
-            cv::imshow("card keypoints", output);
-        }
-        if(card.getType() == PokerCard::Type::Clubs && card.getValue() == PokerCard::Value::Nine)
-        {
-            cv::imshow("processedCard", card.getPreprocessedPixelData());
-        }
-    }*/
-
-    pokerAnalyzer.analyze(pokerTables[10]);
-    //cv::imwrite("processedTable.jpg", pokerTables[0].getPreprocessedPixelData());
-
-    //cv::waitKey();
-
-    SolutionAResult results(pokerAnalyzer.getCards());
-    return results;
 }
